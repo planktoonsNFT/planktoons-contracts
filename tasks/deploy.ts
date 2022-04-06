4fb2e63e9a41eb165b531d4bb46a9221f35589b6
Brandon Valosek <bvalosek@gmail.com>
2022-04-06 15:22:02 -0500

    add hh tasks

---
+++ tasks/deploy.ts
@@ -0,0 +1,43 @@
+import { task } from "hardhat/config";
+import { readDeploymentsFile, writeDeploymentsFile } from "./file";
+
+task("deploy", "Deploy a contract")
+  .addParam<string>("contract", "Contract to deploy")
+  .setAction(async ({ contract }, { ethers, run, network }) => {
+    console.log("compiling all contracts ...");
+    await run("compile");
+
+    const ContractFactory: any = await ethers.getContractFactory(contract);
+
+    console.log(`Deploying ${contract} ...`);
+    const deployed = await ContractFactory.deploy();
+    await deployed.deployed();
+    const address = deployed.address;
+
+    console.log("\n\n---");
+    console.log(`🚀 ${contract}: ${address}`);
+    console.log("---\n\n");
+
+    if (network.name !== "localhost" && network.name !== "hardhat") {
+      console.log("updating factory-deployments.json ...");
+      const entries = await readDeploymentsFile();
+      const entry = entries[network.name] ?? {};
+      entries[network.name] = { ...entry, [contract]: address };
+      await writeDeploymentsFile(entries);
+
+      console.log("waiting 60 seconds before attempting to verify ...");
+      await new Promise((resolve) => setTimeout(resolve, 60 * 1000));
+
+      console.log("verifying...");
+      try {
+        await run("verify:verify", {
+          address: deployed.address,
+          constructorArguments: [],
+        });
+      } catch (err) {
+        console.warn("Verfication error:", err);
+      }
+    }
+
+    return address;
+  });
