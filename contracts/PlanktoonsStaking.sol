1a0567b97a74c8fd883fac02b6d26e53a1e19c19
Brandon Valosek <bvalosek@gmail.com>
2022-04-07 11:28:16 -0500

    add PlanktoonsStaking contract

---
+++ contracts/PlanktoonsStaking.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {NFTStaking} from "./NFTStaking.sol";
+
+contract PlanktoonsStaking is NFTStaking {
+  string constant public name = "PlanktoonsStaking";
+}
