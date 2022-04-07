83115e0c0cc23e7acff618ea435226834f1b8f04
Brandon Valosek <bvalosek@gmail.com>
2022-04-06 20:58:47 -0500

    NFTStaking fixes

--- contracts/NFTStaking.sol
+++ contracts/NFTStaking.sol
@@ -12,8 +12,8 @@ struct AccountStake {
     mapping(uint256 => bool) stakedTokens;
 }
 
-uint256 constant EMPTY = 5000; // sentinel for unused stake slot
-
+/// @notice A simple NFT staking contract to emit deposited reserves to stakers
+/// as claimable tokens.
 contract NFTStaking is Ownable {
     /// @notice The amount of tokens that are emitted per day per NFT.
     uint256 public immutable DAILY_RATE = 1 ether;
@@ -27,7 +27,7 @@ contract NFTStaking is Ownable {
     /// @notice Staking contract was already setup.
     error AlreadySetup();
 
-    /// @notice An invalid NFT was attempted to be unstaked
+    /// @notice An invalid NFT was attempted to be unstaked.
     error InvalidNFT();
 
     mapping(address => AccountStake) private _stakes;
@@ -53,13 +53,9 @@ contract NFTStaking is Ownable {
         }
     }
 
-    /// @notice Returns the total claimable tokens for a given account.
-    function getClaimable(address account) public view returns (uint256) {
-        uint256 delta = block.timestamp - _stakes[account].lastClaimTime;
-        uint256 emitted = (_stakes[account].stakedCount * DAILY_RATE * delta) /
-            1 days;
-        return emitted + _stakes[account].earned;
-    }
+    // ---
+    // Holder functionality
+    // ---
 
     /// @notice Stake multiple NFTs
     function stakeNFTs(uint256[] memory tokenIds) external {
@@ -77,7 +73,7 @@ contract NFTStaking is Ownable {
     /// @notice Claim all earned tokens for msg.sender
     function claim() public {
         uint256 claimable = getClaimable(msg.sender);
-        token.transferFrom(address(this), msg.sender, claimable); // reverts if insufficient balance
+        token.transfer(msg.sender, claimable); // reverts if insufficient balance
         _stakes[msg.sender].lastClaimTime = block.timestamp;
         _stakes[msg.sender].earned = 0;
         // TODO: event
@@ -94,5 +90,17 @@ contract NFTStaking is Ownable {
             delete _stakes[msg.sender].stakedTokens[tokenId];
             nft.transferFrom(address(this), msg.sender, tokenId);
         }
+    }
+
+    // ---
+    // Views
+    // ---
+
+    /// @notice Returns the total claimable tokens for a given account.
+    function getClaimable(address account) public view returns (uint256) {
+        uint256 delta = block.timestamp - _stakes[account].lastClaimTime;
+        uint256 emitted = (_stakes[account].stakedCount * DAILY_RATE * delta) /
+            1 days;
+        return emitted + _stakes[account].earned;
     }
 }
