0f840e6241e34cb6b479d751b53c0c7dc793d053
Brandon Valosek <bvalosek@gmail.com>
2022-04-06 20:58:53 -0500

    add NFTStaking test

---
+++ test/NFTStaking.spec.ts
@@ -0,0 +1,52 @@
+import { ethers } from "hardhat";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { MockERC721, MockERC20, NFTStaking } from "../typechain";
+import { parseUnits } from "ethers/lib/utils";
+
+describe("basic staking and unstaking", () => {
+  describe("NFTStaking", () => {
+    // ---
+    // fixtures
+    // ---
+
+    let nft: MockERC721;
+    let token: MockERC20;
+    let staking: NFTStaking;
+    let accounts: SignerWithAddress[];
+    let a0: string, a1: string, a2: string, a3: string;
+
+    beforeEach(async () => {
+      const MockERC20 = await ethers.getContractFactory("MockERC20");
+      const MockERC721 = await ethers.getContractFactory("MockERC721");
+      const NFTStaking = await ethers.getContractFactory("NFTStaking");
+      [nft, token, staking, accounts] = await Promise.all([
+        MockERC721.deploy(),
+        MockERC20.deploy(),
+        NFTStaking.deploy(),
+        ethers.getSigners(),
+      ]);
+      const initialDeposit = parseUnits("1000000");
+      await token.mint(initialDeposit);
+      await token.approve(staking.address, parseUnits("10000000000000"));
+      await nft.setApprovalForAll(staking.address, true);
+      await staking.setup(nft.address, token.address, initialDeposit);
+      [a0, a1, a2, a3] = accounts.map((a) => a.address);
+    });
+    it("should support staking and unstaking a single NFT", async () => {
+      const tokenId = "1";
+      await nft.mint(tokenId);
+      await staking.stakeNFTs([tokenId]);
+      await staking.claimAndUnstakeNFTs([tokenId]);
+      //
+    });
+    it("should revert if staking an unowned NFT", async () => {
+      //
+    });
+    it("should revert if unstaking an unstaked NFT", async () => {
+      //
+    });
+    it("should revert if unstaking an NFT staked by a different address", async () => {
+      //
+    });
+  });
+});
