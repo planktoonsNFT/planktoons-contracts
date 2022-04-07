c1760ea7bed26d15e7a42ee20a3d37f59c7d50eb
Brandon Valosek <bvalosek@gmail.com>
2022-04-06 20:58:38 -0500

    add mock tokens

---
+++ contracts/mocks/MockERC721.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MockERC721 is ERC721 {
+    constructor() ERC721("MockERC721", "TEST") {}
+
+    function mint(uint256 tokenId) external {
+        _mint(_msgSender(), tokenId);
+    }
+
+    function burn(uint256 tokenId) external {
+        _burn(tokenId);
+    }
+}
