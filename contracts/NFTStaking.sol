a610ddb0e196b8e05b66e01d5978c7f7f1622ee4
Brandon Valosek <bvalosek@gmail.com>
2022-04-06 21:09:25 -0500

    add events

--- contracts/NFTStaking.sol
+++ contracts/NFTStaking.sol
@@ -18,19 +18,40 @@ contract NFTStaking is Ownable {
     /// @notice The amount of tokens that are emitted per day per NFT.
     uint256 public immutable DAILY_RATE = 1 ether;
 
+    // ---
+    // Storage
+    // ---
+
     /// @notice The NFT that can be staked in this contract.
     IERC721 public nft;
 
     /// @notice The token that is emitted from this contract.
     IERC20 public token;
 
+    mapping(address => AccountStake) private _stakes;
+
+    // ---
+    // Events
+    // ---
+
+    /// @notice An NFT was staked into the contract.
+    event NFTStaked(address owner, uint256 tokenId);
+
+    /// @notice An NFT was unstaked from the contract.
+    event NFTUnstaked(address owner, uint256 tokenId);
+
+    /// @notice Tokens were claimed.
+    event TokensClaimed(address owner, uint256 amount);
+
+    // ---
+    // Errors
+    // ---
+
     /// @notice Staking contract was already setup.
     error AlreadySetup();
 
     /// @notice An invalid NFT was attempted to be unstaked.
     error InvalidNFT();
-
-    mapping(address => AccountStake) private _stakes;
 
     // ---
     // Admin functionality
@@ -49,7 +70,6 @@ contract NFTStaking is Ownable {
 
         if (deposit_ > 0) {
             token.transferFrom(msg.sender, address(this), deposit_); // reverts if not approved
-            // TODO: event
         }
     }
 
@@ -67,16 +87,17 @@ contract NFTStaking is Ownable {
             uint256 tokenId = tokenIds[i];
             _stakes[msg.sender].stakedTokens[tokenId] = true;
             nft.transferFrom(msg.sender, address(this), tokenId); // reverts if not approved
+            emit NFTStaked(msg.sender, tokenId);
         }
     }
 
     /// @notice Claim all earned tokens for msg.sender
     function claim() public {
         uint256 claimable = getClaimable(msg.sender);
-        token.transfer(msg.sender, claimable); // reverts if insufficient balance
         _stakes[msg.sender].lastClaimTime = block.timestamp;
         _stakes[msg.sender].earned = 0;
-        // TODO: event
+        token.transfer(msg.sender, claimable); // reverts if insufficient balance
+        emit TokensClaimed(msg.sender, claimable);
     }
 
     /// @notice Claim all unearned tokens and unstake a subset of staked NFTs
@@ -89,6 +110,7 @@ contract NFTStaking is Ownable {
             if (!_stakes[msg.sender].stakedTokens[tokenId]) revert InvalidNFT();
             delete _stakes[msg.sender].stakedTokens[tokenId];
             nft.transferFrom(address(this), msg.sender, tokenId);
+            emit NFTUnstaked(msg.sender, tokenId);
         }
     }
 
