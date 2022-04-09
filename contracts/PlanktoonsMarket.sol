e74a59ab2acb9c24827b3e77154effcb7cee20bb
Brandon Valosek <bvalosek@gmail.com>
2022-04-09 12:16:23 -0500

    add PlanktoonsMarket

---
+++ contracts/PlanktoonsMarket.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {MerkleMarket} from "./MerkleMarket.sol";
+
+contract PlanktoonsMarket is MerkleMarket {
+  string constant public name = "PlanktoonsMarket";
+}
