ee763e50e17d50b4f4562c8ab4ad1ea15c8e461b
Brandon Valosek <bvalosek@gmail.com>
2022-04-11 13:36:14 -0500

    update copy pasta script

--- copy-pasta-abis.sh
+++ copy-pasta-abis.sh
@@ -10,15 +10,17 @@ FRONTEND=../planktoons-frontend/core/abis
 yarn build \
   && cp \
     $OZ_BASE/token/ERC20/IERC20.sol/IERC20.json \
-    $OZ_BASE/token/ERC20/extensions/IERC20Metadata.sol/IERC20Metadata.json \
     $OZ_BASE/token/ERC721/IERC721.sol/IERC721.json \
+    $OZ_BASE/token/ERC20/extensions/IERC20Metadata.sol/IERC20Metadata.json \
     $BASE/NFTStaking.sol/NFTStaking.json \
     $BASE/MerkleMarket.sol/MerkleMarket.json \
+    $BASE/MerkleAirdrop.sol/MerkleAirdrop.json \
       $GRAPH \
   && cp \
     $OZ_BASE/token/ERC20/IERC20.sol/IERC20.json \
-    $OZ_BASE/token/ERC20/extensions/IERC20Metadata.sol/IERC20Metadata.json \
     $OZ_BASE/token/ERC721/IERC721.sol/IERC721.json \
+    $OZ_BASE/token/ERC20/extensions/IERC20Metadata.sol/IERC20Metadata.json \
     $BASE/NFTStaking.sol/NFTStaking.json \
     $BASE/MerkleMarket.sol/MerkleMarket.json \
+    $BASE/MerkleAirdrop.sol/MerkleAirdrop.json \
       $FRONTEND
