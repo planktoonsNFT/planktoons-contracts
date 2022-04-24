2f05a8c92646163b4a614966ba282aefe9352331
Brandon Valosek <bvalosek@gmail.com>
2022-04-24 16:42:37 -0500

    add more staking tests

--- test/NFTStaking.spec.ts
+++ test/NFTStaking.spec.ts
@@ -4,7 +4,7 @@ import { MockERC721, MockERC20, NFTStaking } from "../typechain";
 import { parseUnits } from "ethers/lib/utils";
 import { expect } from "chai";
 
-describe("NFTStaking.sol", () => {
+describe.only("NFTStaking.sol", () => {
   // ---
   // fixtures
   // ---
@@ -37,6 +37,17 @@ describe("NFTStaking.sol", () => {
     await nft.setApprovalForAll(staking.address, true);
   });
 
+  const mineNextBlock = () => ethers.provider.send("evm_mine", []);
+  const setAutomine = (set = true) =>
+    ethers.provider.send("evm_setAutomine", [set]);
+
+  const increaseTimestampAndMineNextBlock = async (offsetInSeconds: number) => {
+    await ethers.provider.send("evm_increaseTime", [offsetInSeconds]);
+    await mineNextBlock();
+  };
+
+  afterEach(() => setAutomine(true));
+
   it("should support staking and unstaking a single NFT", async () => {
     const tokenId = "1";
     await nft.mint(tokenId);
@@ -48,19 +59,61 @@ describe("NFTStaking.sol", () => {
     //
   });
   it("should unstaking a subset of staked NFTs", async () => {
-    //
+    await nft.mint("1");
+    await nft.mint("2");
+    await nft.mint("3");
+    expect(await nft.balanceOf(a0)).to.equal(3);
+    await staking.stakeNFTs(["1", "2", "3"]);
+    expect(await staking.getStakedBalance(a0)).to.equal(3);
+    await staking.claimAndUnstakeNFTs(["2"]);
+    expect(await staking.getStakedBalance(a0)).to.equal(2);
+    expect(await nft.balanceOf(a0)).to.equal(1);
+  });
+  it("should allow emergency unstaking of NFTs even when reserves are empty", async () => {
+    const NFTStaking = await ethers.getContractFactory("NFTStaking");
+    const staking = await NFTStaking.deploy();
+    await token.approve(staking.address, parseUnits("1"));
+    await nft.setApprovalForAll(staking.address, true);
+
+    await nft.mint("1");
+    await token.mint(parseUnits("1"));
+    await staking.setup(nft.address, token.address, parseUnits("1"));
+    await staking.stakeNFTs(["1"]);
+    await increaseTimestampAndMineNextBlock(60 * 60 * 24 * 10);
+
+    // not enough reserve tokens
+    await expect(staking.claimAndUnstakeNFTs(["1"])).to.be.revertedWith(
+      "transfer amount exceeds balance"
+    );
+    await staking.emergencyUnstake(["1"]); // doesnt revert
+    expect(await nft.ownerOf("1")).to.equal(a0);
   });
   it("should allow permissionlessly 3rd party claiming", async () => {
-    //
+    await nft.connect(accounts[1]).mint("1");
+    await nft.connect(accounts[1]).setApprovalForAll(staking.address, true);
+    await setAutomine(false);
+    await staking.connect(accounts[1]).stakeNFTs(["1"]);
+    await mineNextBlock();
+    await staking.connect(accounts[2]).claimFor(accounts[1].address);
+    await increaseTimestampAndMineNextBlock(60 * 60 * 24);
+    expect(await token.balanceOf(accounts[1].address)).to.equal(
+      parseUnits("1")
+    );
   });
   it("should revert if staking an unowned NFT", async () => {
-    //
+    await nft.mint("1");
+    await expect(
+      staking.connect(accounts[1]).stakeNFTs(["1"])
+    ).to.be.revertedWith("NotTokenOwner()");
   });
   it("should revert if attempting to set cutoff in the past", async () => {
     //
   });
   it("should revert if duplicate token IDs when staking", async () => {
-    //
+    await nft.mint("1");
+    await expect(
+      staking.connect(accounts[1]).stakeNFTs(["1", "1"])
+    ).to.be.revertedWith("NotTokenOwner()");
   });
   it("should revert if duplicate token IDs when unstaking", async () => {
     //
@@ -71,13 +124,16 @@ describe("NFTStaking.sol", () => {
   it("should revert if unstaking an NFT staked by a different address", async () => {
     //
   });
-  it("should revert setting up with an invalid nft", async () => {
-    //
-  });
-  it("should revert setting up with an invalid token", async () => {
-    //
-  });
   it("should revert setting up more than once", async () => {
-    //
+    await expect(
+      staking.setup(nft.address, token.address, parseUnits("1000"))
+    ).to.be.revertedWith("AlreadySetup()");
+  });
+  it("should revert setting up by non owner", async () => {
+    await expect(
+      staking
+        .connect(accounts[1])
+        .setup(nft.address, token.address, parseUnits("1000"))
+    ).to.be.revertedWith("caller is not the owner");
   });
 });
