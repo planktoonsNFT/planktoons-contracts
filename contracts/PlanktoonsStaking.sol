fc96c7dbdc2a5d9450bc2d1372dee5012c12b724
Brandon Valosek <bvalosek@gmail.com>
2022-04-28 01:48:08 -0500

    staking contract header

--- contracts/PlanktoonsStaking.sol
+++ contracts/PlanktoonsStaking.sol
@@ -1,6 +1,20 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+/*
+
+    ███████╗████████╗ █████╗ ██╗  ██╗██╗███╗   ██╗ ██████╗
+    ██╔════╝╚══██╔══╝██╔══██╗██║ ██╔╝██║████╗  ██║██╔════╝
+    ███████╗   ██║   ███████║█████╔╝ ██║██╔██╗ ██║██║  ███╗
+    ╚════██║   ██║   ██╔══██║██╔═██╗ ██║██║╚██╗██║██║   ██║
+    ███████║   ██║   ██║  ██║██║  ██╗██║██║ ╚████║╚██████╔╝
+    ╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝
+
+              Planktoons NFT staking contract
+                  https://planktoons.io
+
+*/
+
 import {NFTStaking} from "./NFTStaking.sol";
 
 contract PlanktoonsStaking is NFTStaking {
