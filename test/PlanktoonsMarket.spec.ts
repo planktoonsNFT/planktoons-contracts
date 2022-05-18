9dceceb241b87ee17652076382f10954c9380a70
Brandon Valosek <bvalosek@gmail.com>
2022-05-18 17:57:57 -0500

    fix tests

--- test/PlanktoonsMarket.spec.ts
+++ test/PlanktoonsMarket.spec.ts
@@ -1,6 +1,12 @@
 import { ethers } from "hardhat";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { MockERC20, PlanktoonsMarket } from "../typechain";
+import {
+  MerkleAirdrop,
+  MockERC20,
+  MockERC721,
+  NFTStaking,
+  PlanktoonsMarket,
+} from "../typechain";
 import { createMerkleTree } from "./util";
 import { parseUnits } from "ethers/lib/utils";
 import { expect } from "chai";
@@ -11,21 +17,35 @@ describe("PlanktoonsMarket.sol", () => {
   // ---
 
   let token: MockERC20;
+  let nft: MockERC721;
+  let staking: NFTStaking;
+  let airdrop: MerkleAirdrop;
   let market: PlanktoonsMarket;
   let accounts: SignerWithAddress[];
   let a0: string, a1: string, a2: string, a3: string;
 
   beforeEach(async () => {
     const MockERC20 = await ethers.getContractFactory("MockERC20");
+    const MockERC721 = await ethers.getContractFactory("MockERC721");
+    const NFTStaking = await ethers.getContractFactory("NFTStaking");
+    const MerkleAirdrop = await ethers.getContractFactory("MerkleAirdrop");
     const PlanktoonsMarket = await ethers.getContractFactory(
       "PlanktoonsMarket"
     );
-    [token, market, accounts] = await Promise.all([
+    [token, nft, staking, airdrop, accounts] = await Promise.all([
       MockERC20.deploy(),
-      PlanktoonsMarket.deploy(),
+      MockERC721.deploy(),
+      NFTStaking.deploy(),
+      MerkleAirdrop.deploy(),
       ethers.getSigners(),
     ]);
     [a0, a1, a2, a3] = accounts.map((a) => a.address);
+
+    const market = await PlanktoonsMarket.deploy(
+      nft.address,
+      staking.address,
+      airdrop.address
+    );
 
     // market is approved to spend mock token for a0
     await token.approve(market.address, parseUnits("100000000000000000"));
